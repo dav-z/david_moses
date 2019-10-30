b8712f7304ddbd5479782afc21a4e51ada6f2897
David Zheng <davidzheng@knights.ucf.edu>
2019-10-30 19:27:34 -0400

    github api

--- js/main.js
+++ js/main.js
@@ -19,60 +19,25 @@
 
 
 $( window ).on('load', function() {
-  // var currentIndex = 0,
-  // items = $('.container div'),
-  // itemAmt = items.length;
-  //
-  // function cycleItems() {
-  //   var item = $('.container div').eq(currentIndex);
-  //   // items.hide();
-  //   // item.css('display','inline-block');
-  // }
-  //
-  // $('.next').click(function() {
-  //   // clearInterval(autoSlide);
-  //   currentIndex += 1;
-  //   if (currentIndex > itemAmt - 1) {
-  //     currentIndex = 0;
-  //   }
-  //   var container = $(".container");
-  //   var width = $(".container div").width();
-  //   container.scrollLeft(container.scrollLeft() - width);
-  //   // cycleItems();
-  // });
-  //
-  // $('.prev').click(function() {
-  //   // clearInterval(autoSlide);
-  //   currentIndex -= 1;
-  //   if (currentIndex < 0) {
-  //     currentIndex = itemAmt - 1;
-  //   }
-  //   var container = $(".container");
-  //   var width = $(".container div").width();
-  //   container.scrollLeft(container.scrollLeft() + width);
-  //   // cycleItems();
-  // });
-
-
-  var gallery = $('#gallery ul'),
-      items   = gallery.find('li'),
-      len     = items.length,
-      current = 1,  /* the current item we're looking */
-
-      first   = items.filter(':first-child'),
-      second  = items.filter((index)=>index==1),
-      last    = items.filter(':last-child'),
-      secondlast = items.filter((index)=>index==items.length-2),
-
+//   var gallery = $('#gallery ul'),
+//       items   = gallery.find('li'),
+//       // len     = items.length,
+//       current = 1,  /* the current item we're looking */
+//
+//       first   = items.filter(':first-child'),
+//       second  = items.filter((index)=>index==1),
+//       last    = items.filter(':last-child'),
+//       secondlast = items.filter((index)=>index==items.length-2),
+// //
       triggers = $('.button');
-
-console.log("second 1 " + second);
-
-  /* 1. Cloning first and last item */
-  first.before(secondlast.clone(true));
-  first.before(last.clone(true));
-  last.after(second.clone(true));
-  last.after(first.clone(true));
+//
+// console.log("second 1 " + second);
+//
+//   /* 1. Cloning first and last item */
+//   first.before(secondlast.clone(true));
+//   first.before(last.clone(true));
+//   last.after(second.clone(true));
+//   last.after(first.clone(true));
 
   /* 2. Set button handlers */
   triggers.on('click', function() {
@@ -86,6 +51,7 @@ console.log("second 1 " + second);
         gallery.animate({ left: "+=" + (-width * delta) }, function() {
 
             current += delta;
+            console.log(current);
 
             /**
              * we're cycling the slider when the the value of "current"
@@ -93,7 +59,7 @@ console.log("second 1 " + second);
              * the initial gallery length
              */
             cycle = !!(current === 0 || current > len);
-
+            console.log(len);
             if (cycle) {
                 /* we switched from image 1 to 4-cloned or
                    from image 4 to 1-cloned */
